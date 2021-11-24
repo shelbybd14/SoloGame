7cf85d5d8fb3cab1e677638726a3943f9ef11537
shelbybd14 <shelbyb.d12@gmail.com>
2021-11-24 02:28:36 +0000

    Lives/Health Working

--- Solo Game/Assets/Code/Player/Health/Health.cs
+++ Solo Game/Assets/Code/Player/Health/Health.cs
@@ -5,26 +5,35 @@ public class Health : MonoBehaviour
     [SerializeField] private float startingHealth;
 
     public float currentHealth { get; private set; }
+    private Animator anim;
+    private bool dead;
 
     private void Awake()
     {
         currentHealth = startingHealth;
+        anim = GetComponent<Animator>();
     }
     public void TakeDamage (float _damage)
     {
         currentHealth = Mathf.Clamp(currentHealth - _damage, 0, startingHealth);
         if(currentHealth >0)
         {
-            //player hurt
+            anim.SetTrigger("hit");
         }
         else
         {
-            //player dead
+            if(!dead)
+            {
+            anim.SetTrigger("dead");
+            GetComponent<CharacterMovement>().enabled = false;
+                dead = true;
+            }
+            
         }
     }
-    private void Update()
+
+    public void AddHealth(float _value)
     {
-        if (Input.GetKeyDown(KeyCode.E))
-            TakeDamage(1);
+        currentHealth = Mathf.Clamp(currentHealth + _value, 0, startingHealth);
     }
 }
